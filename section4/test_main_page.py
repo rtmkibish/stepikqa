7e9027af0fdee6fbe5ca60871cef9cdb8fd44ad0
artem <rtmkibish@gmail.com>
2021-03-12 18:50:53 +0200

    Moving between different pages in one test [skip ci]

--- section4/test_main_page.py
+++ section4/test_main_page.py
@@ -7,9 +7,6 @@ def test_guest_can_go_to_login_page(browser):
   page = MainPage(link, browser)
   page.open()
   page.should_be_login_link()
-
-def test_guest_see_login_page(browser):
-  link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
-  page = LoginPage(link, browser)
-  page.open()
-  page.should_be_login_page()
+  page.go_to_login_page()
+  login_page = LoginPage(browser.current_url, browser)
+  login_page.should_be_login_page()
