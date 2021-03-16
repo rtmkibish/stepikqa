d6ecc9bf51c3b8b1107ca3bafccfebc6cc0fd337
artem <rtmkibish@gmail.com>
2021-03-16 20:24:19 +0200

    Product page test improvments [skip ci]

--- section4/pages/product_page.py
+++ section4/pages/product_page.py
@@ -1,4 +1,4 @@
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException
 
 from pages.base_page import BasePage
 from pages.locators import ProductPageLocators
@@ -13,13 +13,25 @@ class ProductPage(BasePage):
     add_product_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
     add_product_to_basket.click()
 
-  def should_be_product_addition_massage_alert(self):
+  def should_be_added_correct_product_name(self):
     try:
-      self.browser.switch_to.alert
-    except NoAlertPresentException:
-      raise AssertionError("Product added to the basket alert message should be presented")
+      message_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_MESSAGE).text
+    except NoSuchElementException:
+      raise AssertionError("Correct product name should be presented in the message")
+
+    product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+    assert message_product_name == product_name, "Product name should be the same in the message"
 
   def should_basket_price_equal_to_product_price(self):
     product_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
     basket_total_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
     assert basket_total_price.strip() == product_price
+
+  def should_message_price_be_equal_to_product_price(self):
+    product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+    try:
+      message_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_MESSAGE_PRICE).text
+    except NoSuchElementException:
+      raise AssertionError("Correct product price should be presented in the message")
+
+    assert product_price == message_product_price, "Product price should be the same in the message"
