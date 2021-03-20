75e2e4b11c8f68ebbc0bcd3985bd3bb7f41a63dc
artem <rtmkibish@gmail.com>
2021-03-20 20:47:05 +0200

    Login link related method in the BasePage

--- section4/pages/main_page.py
+++ section4/pages/main_page.py
@@ -1,7 +1,4 @@
 from pages.base_page import BasePage
-from pages.locators import MainPageLocators
-
-from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
@@ -9,9 +6,5 @@ class MainPage(BasePage):
   Represents the Main page
   """
 
-  def go_to_login_page(self):
-    login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-    login_link.click()
-
-  def should_be_login_link(self):
-    assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
--- section4/test_product_page.py
+++ section4/test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -53,3 +54,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
   product_page.add_product_to_basket()
   product_page.solve_quiz_and_get_code()
   product_page.is_product_message_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+  link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+  page = ProductPage(browser, link)
+  page.open()
+  page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+  link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+  page = ProductPage(link, browser)
+  page.open()
+  page.go_to_login_page()
+  login_page = LoginPage(browser.current_url, browser)
+  login_page.is_on_page()
